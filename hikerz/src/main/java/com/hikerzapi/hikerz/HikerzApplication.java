35c48d59e73301821bdf59231c438d3419eabb2d
Moustapha Kebe <moustaphakebe@Moustaphas-MacBook-Air.local>
2023-09-05 21:23:34 +0000

    project creation

---
+++ hikerz/src/main/java/com/hikerzapi/hikerz/HikerzApplication.java
@@ -0,0 +1,13 @@
+package com.hikerzapi.hikerz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikerzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikerzApplication.class, args);
+	}
+
+}
---
+++ hikerz/src/test/java/com/hikerzapi/hikerz/HikerzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hikerzapi.hikerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikerzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

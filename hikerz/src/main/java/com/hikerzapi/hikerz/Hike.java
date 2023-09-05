bdabf5e45288e47ba2f1a109945036c12fa55c59
Moustapha Kebe <moustaphakebe@Moustaphas-MacBook-Air.local>
2023-09-05 23:01:48 +0000

    adding the Hike and Review classes

---
+++ hikerz/src/main/java/com/hikerzapi/hikerz/Hike.java
@@ -0,0 +1,29 @@
+package com.hikerzapi.hikerz;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "hikes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Hike {
+    @Id
+    private ObjectId id;
+    private String title;
+    private String publishDate;
+    private String videoLink;
+    private List<String> types;
+    private String country;
+    private String mainImage;
+    private List<String> extraImages;
+    @DocumentReference
+    private List<Review> reviewIds;
+}
